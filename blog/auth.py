80b0030b94ec6547671e686741d8581179b0528f
Mario Krajacic <krajacic.mario@gmail.com>
2020-08-25 15:05:33 +0200

    Added login route and method

--- blog/auth.py
+++ blog/auth.py
@@ -10,6 +10,7 @@ from blog.db import get_db
 bp = Blueprint('auth', __name__, url_prefix='/auth')
 
 
+# Register route
 @bp.route('/register', methods=('GET', 'POST'))
 def register():
     if request.method == 'POST':
@@ -38,3 +39,30 @@ def register():
         flash(error)
 
     return render_template('auth/register.html')
+
+
+# Login route
+@bp.route('/login', methods=('GET', 'POST'))
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        db = get_db()
+        error = None
+        user = db.execute(
+            'SELECT * FROM user WHERE username = ?', (username,)
+        ).fetchone()
+
+        if user is None:
+            error = 'Incorrect username.'
+        elif not check_password_hash(user['password'], password):
+            error = 'Incorrect password.'
+
+        if error is None:
+            session.clear()
+            session['user_id'] = user['id']
+            return redirect(url_for('index'))
+
+        flash(error)
+
+    return render_template('auth/login.html')
